35aab095c3b91e4416b00ab609c76909e2cd65a5
TetianaPotochna <t.potochna@gmail.com>
2025-08-04 20:34:40 +0300

    Login successes

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,12 +3,19 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class ApplicationManager {
     WebDriver wd;
+    HelperUser helperUser;
 
     public void init() {
         wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
 
     }
 
@@ -16,4 +23,7 @@ public class ApplicationManager {
         wd.quit();
     }
 
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,39 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SignOut present ---> Logout
+        if (app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
-    public void test(){
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("dusm6@gmail.com", "Tt123456$");
 
+        app.getHelperUser().submitLogin();
+
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("dusm6@gmail.com", "Tt123456$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
